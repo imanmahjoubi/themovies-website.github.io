826930e9e9029857cefc86a37ee10dc9537db603
Imen Mahjoubi <imen.mahjoubi@esprit.tn>
2018-07-14 10:19:40 -0700

    card add

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDNLV8nEiHGle0AeKXrvoCbcpLxZjLSCng",
+        authDomain: "iheartmovie-e6ca6.firebaseapp.com",
+        databaseURL: "https://iheartmovie-e6ca6.firebaseio.com",
+        projectId: "iheartmovie-e6ca6",
+        storageBucket: "iheartmovie-e6ca6.appspot.com",
+        messagingSenderId: "847005601179"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
+
     $('#demo-carousel').carousel();
     //set time for auto (otherwise remove this part)
     setInterval(function(){
@@ -12,4 +27,5 @@ $(document).ready(function(){
         autoplaySpeed: 3000
     });
 });
+
 
